2cd3d21b52453f0d6bc65ae750d3115fc3bc67bb
Raul Sanchez R. <raul.sr92@gmail.com>
2026-06-08 13:54:35 -0500

    Clase 107 - Rutas y estructura de carpetas

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,15 @@
+
+import { Routes } from '@angular/router';
+import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
+
+export const countryRoutes: Routes = [
+
+  {
+    path: '',
+    component: ByCapitalPage,
+  }
+
+];
+
+
+export default countryRoutes
