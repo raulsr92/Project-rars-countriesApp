e5aea85da1ab2d94df2f1aa897dbb126408ff0ca
Raul Sanchez R. <raul.sr92@gmail.com>
2026-06-08 16:12:37 -0500

    Clase 108 - Layout Components

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,12 +1,23 @@
 
 import { Routes } from '@angular/router';
 import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
+import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
 
 export const countryRoutes: Routes = [
 
   {
     path: '',
-    component: ByCapitalPage,
+    component: CountryLayout,
+    children:[
+      {
+        path: "by-capital",
+        component: ByCapitalPage
+      },
+      {
+        path:'**',
+        redirectTo: 'by-capital'
+      }
+    ]
   }
 
 ];
