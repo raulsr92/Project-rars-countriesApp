6b578c31f0d5d38ce85465f6e29173879f02357c
Raul Sanchez R. <raul.sr92@gmail.com>
2026-06-15 14:21:20 -0500

    Clase 110 Menu superior e iocnos

--- src/app/country/layouts/CountryLayout/CountryLayout.ts
+++ src/app/country/layouts/CountryLayout/CountryLayout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { TopMenu } from "../../components/top-menu/top-menu";
 
 @Component({
   selector: 'app-country-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopMenu, RouterLink],
   templateUrl: './CountryLayout.html',
 })
 export class CountryLayout {}
